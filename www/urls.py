ade6a634ff5472ce4bbe084163c43a01398be436
Jaeman An <kuss@sparcs.kaist.ac.kr>
2012-08-23 02:29:25 +0900

    디렉토리 구조 정리

---
+++ www/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
+    (r'^register/$','accounts.views.register'),
+    (r'^login/$','accounts.views.login_page'),
+    (r'^logout/$','accounts.views.logout'),
+    
+    # Media Root
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': './media'}),
+    # Examples:
+    # url(r'^$', 'trihack.views.home', name='home'),
+    # url(r'^trihack/', include('trihack.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
