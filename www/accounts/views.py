5a4b284618fe460c2be1823f86dee75021865b8b
zzongaly <zzongaly@bit.sparcs.org>
2012-08-23 21:51:17 +0900

    register 에서 expertise를 항목 리스트로 저장

--- www/accounts/views.py
+++ www/accounts/views.py
@@ -95,7 +95,7 @@ def register(request):
         
         if not error :
             f = open("../result.txt","a+")
-            f.write(' / '.join([team,email,contact,str(expertise_check),members_name1,members_name2,members_name3,members_university1,members_university2,members_university3,country])+"\n")
+            f.write(' / '.join([team,email,contact,str(expertise_list),members_name1,members_name2,members_name3,members_university1,members_university2,members_university3,country])+"\n")
             f.close()
             return render(request,'register_success.html',{})
         
