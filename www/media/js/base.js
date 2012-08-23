6ec44047b538a168daf954dc32860951d1c92d29
Jaeman An <kuss@sparcs.kaist.ac.kr>
2012-08-24 04:27:32 +0900

    login 페이지 작성

--- www/media/js/base.js
+++ www/media/js/base.js
@@ -2,6 +2,8 @@ var MainNav = {
 	initialize:function()
 	{
 		this.menu_register = $("#menu_register a");
+		this.menu_login = $("#menu_login a");
+		this.menu_logout = $("#menu_logout a");
 		this.menu_about = $("#menu_about a");
 		this.menu_forum = $("#menu_forum a");
 
@@ -36,6 +38,22 @@ var MainNav = {
 				$("#menu_forum a").css('background', 'url(/media/image/forum.png) no-repeat top left');
 			});
 		}
+
+		if (this.loc != "login") {
+			this.menu_login.bind('mouseenter', function() {
+				$("#menu_login a").css('background', 'url(/media/image/login_active.png) no-repeat top left');
+			});
+			this.menu_login.bind('mouseleave', function() {
+				$("#menu_login a").css('background', 'url(/media/image/login.png) no-repeat top left');
+			});			
+		}
+
+		this.menu_logout.bind('mouseenter', function() {
+			$("#menu_logout a").css('background', 'url(/media/image/logout_active.png) no-repeat top left');
+		});
+		this.menu_logout.bind('mouseleave', function() {
+			$("#menu_logout a").css('background', 'url(/media/image/logout.png) no-repeat top left');
+		});			
 	},
 	setNavImages:function()
 	{
@@ -50,6 +68,10 @@ var MainNav = {
 		else if(location.href.indexOf("/forum/") != -1){
 			this.loc = "forum";
 			this.menu_forum.css('background','url(/media/image/forum_active.png) no-repeat top left');
+		}
+		else if(location.href.indexOf("/login/") != -1){
+			this.loc = "login";
+			this.menu_login.css('background','url(/media/image/login_active.png) no-repeat top left');
 		}
 	}
 }
