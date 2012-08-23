7b546736bef048cb3a43365aef3cda3b775e777a
zzongaly <zzongaly@bit.sparcs.org>
2012-08-23 17:07:33 +0900

    register 페이지에 맞게 views.py 수정
    success는 아님, 에러메세지 및 입력 유지

--- www/accounts/views.py
+++ www/accounts/views.py
@@ -33,56 +33,103 @@ def logout(request):
     return HttpResponseRedirect('/')
 
 def register(request):
-    nameT = "Name"
-    teamT = "Team name"
-    emailT = "Email"
-    locationT = 'Location' 
-    nameE = teamE = emailE = locationE =''
-    name = team = email = location = ''
-    checked = None
+    teamE = emailE = contactE = members_name1E = members_name2E = members_name3E = members_university1E = members_university2E = members_university3E = countryE = ''
+    team = email = contact = members_name1 = members_name2 = members_name3 = members_university1 = members_university2 = members_university3 = country = ''
     error = False
+    
+    expertise = []
+    expertise_list = ['web', 'ios', 'android', 'ui/ux design', 'front-end', 'back-end', 'systems', 'security', 'database']
+    expertise_check = [0 for i in expertise_list]
+    
+    country_list = ['KOREA', 'JAPAN', 'CHINA']
+    country_check = [0 for i in country_list]
+    
     if request.POST:
-        name = request.POST.get('entry.0.single')
-        team = request.POST.get('entry.3.single')
-        email = request.POST.get('entry.1.single')
-        location = request.POST.get('entry.2.group')
-        if name.strip() == '' :
-            nameE = "There is no name" 
-            nameT = ""
-            error = True
-        if team.strip() == '' :
+        team = request.POST.get('team')
+        email = request.POST.get('email')
+        contact = request.POST.get('contact')
+        expertise = request.POST.getlist('expertise')
+
+        members_name1 = request.POST.get('members_name1')
+        members_name2 = request.POST.get('members_name2')
+        members_name3 = request.POST.get('members_name3')
+
+        members_university1 = request.POST.get('members_university1')
+        members_university2 = request.POST.get('members_university2')
+        members_university3 = request.POST.get('members_university3')
+       
+        country = request.POST.get('country')
+
+        if team == None or team.strip() == '' :
             teamE = "There is no team name"
-            teamT = ""
             error = True
-        if email.strip() == '' :
+        if email == None or email.strip() == '' :
             emailE = "There is no Email address"
-            emailT =""
             error = True
         elif not validateEmail(email.strip()) :
             emailE = "Invalid Email Address"
-            emailT = "" 
             error = True
-        if location == None or  location.strip() == '':
-            locationE = "There is no Location information" 
-            locationT = ""
+        if contact == None or contact.strip() == '':
+            contactE = "There is no contact"
             error = True
-       
+        
+        for i in range(len(expertise_list)):
+            if expertise_list[i] in expertise:
+                expertise_check[i] = 1
+
+        for i in range(len(country_list)):
+            if country_list[i] == country:
+                country_check[i] = 1
+        
+        if members_name1 == None or members_name1.strip() == '':
+            members_name1E = "There is no first member name"
+            error = True
+        if members_name2 == None or members_name2.strip() == '':
+            members_name2E = "There is no second member name"
+            error = True
+        if members_name3 == None or members_name3.strip() == '':
+            members_name3E = "There is no third member name"
+            error = True
+        
+        if members_university1 == None or members_university1.strip() == '':
+            members_university1E = "There is no first member university"
+            error = True
+        if members_university2 == None or members_university2.strip() == '':
+            members_university2E = "There is no second member university"
+            error = True
+        if members_university3 == None or members_university3.strip() == '':
+            members_university3E = "There is no third member university"
+            error = True
+
+        if country == None:
+            countryE = 'There is no country'
+            error = True
+        
         if not error :
             return render(request,'register_success.html',{})
         
     return render(request,'register.html',
-            {'nameT':nameT,
-            'teamT':teamT,
-            'emailT':emailT,
-            'locationT':locationT,
-            'nameE':nameE,
-            'teamE':teamE,
-            'emailE':emailE,
-            'locationE':locationE,
-            'name':name,
-            'team':team,
-            'email':email,
-            'location':location})
+            {
+            'team':team, 'teamE':teamE,
+            'email':email, 'emailE':emailE,
+            'contact':contact, 'contactE':contactE,
+            'expertise_check': expertise_check,
+            'members_name1':members_name1, 
+            'members_name1E':members_name1E, 
+            'members_name2':members_name2, 
+            'members_name2E':members_name2E, 
+            'members_name3':members_name3, 
+            'members_name3E':members_name3E, 
+            'members_university1':members_university1, 
+            'members_university1E':members_university1E, 
+            'members_university2':members_university2, 
+            'members_university2E':members_university2E, 
+            'members_university3':members_university3, 
+            'members_university3E':members_university3E, 
+            'country': country,
+            'countryE': countryE,
+            'country_check': country_check,
+            })
 
 def validateEmail(email):
     if len(email) > 7:
