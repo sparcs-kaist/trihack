2d23d9e241b932129504081453de073737165d82
Jaeman An <kuss@sparcs.kaist.ac.kr>
2012-08-24 05:18:00 +0900

    views.py 수정

--- www/forum/models.py
+++ www/forum/models.py
@@ -5,22 +5,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
-	name = models.TextField()
+    name = models.TextField()
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class Post(models.Model):
-	title = models.CharField(u"제목", max_length=100)
-	user = models.ForeignKey(User, null=False)
-	created_on = models.DateTimeField(auto_now=True)
-	modified_on = models.DateTimeField(auto_now=True)
-	text = models.TextField(u"내용")
-	category = models.ForeignKey(Category, null=False, verbose_name=u"게시판")
-	hits = models.IntegerField(null=True, default=0, blank=True)
-	like = models.IntegerField(null=True, default=0, blank=True)
-	hate = models.IntegerField(null=True, default=0, blank=True)
-	commentnumber = models.IntegerField(null=True, default = 0, blank=True)
+    title = models.CharField(u"제목", max_length=100)
+    user = models.ForeignKey(User, null=False)
+    created_on = models.DateTimeField(auto_now=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    text = models.TextField(u"내용")
+    category = models.ForeignKey(Category, null=False, verbose_name=u"게시판")
+    hits = models.IntegerField(null=True, default=0, blank=True)
+    like = models.IntegerField(null=True, default=0, blank=True)
+    hate = models.IntegerField(null=True, default=0, blank=True)
+    commentnumber = models.IntegerField(null=True, default = 0, blank=True)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
--- www/forum/views.py
+++ www/forum/views.py
@@ -5,6 +5,7 @@ from forum.models import *
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.comments.models import Comment
 
@@ -12,104 +13,107 @@ from django.contrib.comments.models import Comment
 rowsPerPage = 7
 
 def showlist(request):
-	if 'category' in request.GET:
-		pk = request.GET['category']
-	else:
-		pk = 'notice'
+    pk = request.GET.get('category', 'notice')
+    boardList = Paginator(Post.objects.filter(category__name = pk).order_by('-id'), rowsPerPage)
+    page = int(request.GET.get('page', 1))
 
-	boardList = Paginator(Post.objects.filter(category__name = pk).order_by('-id'), rowsPerPage)
+    # paging
+    try:
+        contacts = boardList.page(page)
+    except PageNotAnInteger:
+        contatcs = boardList.page(1)
+    except EmptyPage:
+        contacts = boardList.page(boardList.num_pages)    
 
-	if 'page' in request.GET:
-		page = request.GET.get('page')
-	else:
-		page = 1
-# paging
-	try:
-		contacts = boardList.page(page)
-	except PageNotAnInteger:
-		contatcs = boardList.page(1)
-	except EmptyPage:
-		contacts = boardList.page(boardList.num_pages)	
+    if pk == 'notice':
+        title = "NOTICE"
+    else:
+        title = "FREE BOARD"
 
-	if pk == 'notice':
-		title = "NOTICE"
-	else:
-		title = "FREE BOARD"
+    totalCnt = Post.objects.filter(category__name = pk).count()
+    current_page = page
 
-	totalCnt = Post.objects.filter(category__name = pk).count()
-	current_page = page
-
-	return render(request, 'forum_list.html', {'boardList':boardList.page(page), 'contacts':contacts, 'title':title, 'totalCnt':totalCnt, 'current_page':current_page, 'category':pk, 'user':request.user})
+    return render(request, 'forum_list.html', {'boardList':boardList.page(page), 'contacts':contacts, 'title':title, 'totalCnt':totalCnt, 'current_page':current_page, 'category':pk, 'user':request.user})
 
 
 def view_work(request):
-	if request.method == 'post':
-		pass
-	else:
-		pk = request.GET['id']
-		category = request.GET['category']
-		postData = Post.objects.get(id = pk)
-
-		Post.objects.filter(id = pk).update(hits = postData.hits + 1)
-		return render(request, 'forum_view.html', {'postData':postData, 'user':request.user, 'category':category, 'path':request.get_full_path()})
+    if request.method == 'post':
+        pass
+    else:
+        pk = int(request.GET.get('id', -1))
+        category = request.GET.get('category', '')
+        postData = Post.objects.get(id = pk)
+        postData.hits=postData.hits+1
+        postData.save()
+#        Post.objects.filter(id = pk).update(hits=postData.hits + 1)
+        return render(request, 'forum_view.html', {'postData':postData, 'user':request.user, 'category':category, 'path':request.get_full_path()})
 
 
 def write_work(request):
-	pk = request.GET['category']
-	return render(request, 'forum_write.html', {'category':pk, 'user':request.user})
+    pk = request.GET.get('category', '')
+    return render(request, 'forum_write.html', {'category':pk, 'user':request.user})
 
 
+@login_required
 @csrf_exempt
 def dowrite(request):
-	dr = Category.objects.get( name = request.POST['category'])
-	# Create Post
-	br = Post( title = request.POST['title'],
-			created_on = timezone.now(),
-			modified_on = timezone.now(),
-			user = request.user,
-			text = request.POST['contents'],
-			hits = 0,
-			like = 0,
-			hate = 0,
-			commentnumber = 0,
-			category = dr)
-	br.save()
+    category = request.POST.get('category', '')
+    dr = Category.objects.get(name=category)
+    # Create Post
+    br = Post( title = request.POST['title'],
+            created_on = timezone.now(),
+            modified_on = timezone.now(),
+            user = request.user,
+            text = request.POST['contents'],
+            hits = 0,
+            like = 0,
+            hate = 0,
+            commentnumber = 0,
+            category = dr)
+    br.save()
 
-	url = '/forum?category=' + request.POST['category']
-	return HttpResponseRedirect(url)
+    url = '/forum?category=' + category
+    return HttpResponseRedirect(url)
 
-
+@login_required
 def modify_work(request):
-	pk = request.GET['id']
-	category = request.GET['category']
-	postData = Post.objects.get(id = pk)
-	
-	return render(request, 'forum_modify.html', {'postData':postData, 'user':request.user, 'category':category})
+    pk = int(request.GET.get('id', -1))
+    category = request.GET['category']
+    postData = Post.objects.get(id = pk)
+    if postData.user != request.user:
+        return HttpResponseBadRequest()
+    return render(request, 'forum_modify.html', {'postData':postData, 'user':request.user, 'category':category})
 
 
+@login_required
 @csrf_exempt
 def domodify(request):
-	pk = request.POST['id']
-	category = request.POST['category']
-	postData = Post.objects.get(id = pk)
-	
-	postData.modified_on = timezone.now()
-	postData.title = request.POST['title']
-	postData.text = request.POST['contents']
-	
-	dr = Category.objects.get( name = request.POST['category'])
-	postData.category = dr
-	postData.save()
+    pk = int(request.POST.get('id', -1))
+    category = request.POST.get('category', '')
+    postData = Post.objects.get(id=pk)
+    if postData.user != request.user:
+        return HttpResponseBadRequest()
+    
+    postData.modified_on = timezone.now()
+    postData.title = request.POST['title']
+    postData.text = request.POST['contents']
+    
+    dr = Category.objects.get(name=category)
+    postData.category = dr
+    postData.save()
 
-	url = '/forum?category='+ category
-	return HttpResponseRedirect(url)
+    url = '/forum?category='+ category
+    return HttpResponseRedirect(url)
 
-
+@login_required
 def delete_work(request):
-	pk = request.GET['id']
-	category = request.GET['category']
-	postData = Post.objects.get(id = pk)
-	postData.delete()
+    pk = int(request.GET.get('id', -1))
+    category = request.GET.get('category', '')
+    postData = Post.objects.get(id = pk)
+    if postData.user != request.user:
+        return HttpResponseBadReqeust()
 
-	url = '/forum?category=' + category
-	return HttpResponseRedirect(url)
+    postData.delete()
+
+    url = '/forum?category=' + category
+    return HttpResponseRedirect(url)
